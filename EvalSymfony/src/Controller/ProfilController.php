5fd0bfb53ec8694e268433427201c51fc59ed297
L1k3Suzuya <maxime.breu21@gmail.com>
2023-07-28 09:29:38 +0200

    The District symfony

--- EvalSymfony/src/Controller/ProfilController.php
+++ EvalSymfony/src/Controller/ProfilController.php
@@ -11,7 +11,7 @@ class ProfilController extends AbstractController
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
-        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
+        $info = ['lastname' => 'Breu', 'firstname' => ' Maxime', 'email' => 'Maxime@district.fr', 'birthdate' => '23/02/1996'];
 
         return $this->render('profil/index.html.twig', [
             'informations' => $info
